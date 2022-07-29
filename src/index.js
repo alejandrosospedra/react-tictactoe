e279946ead1d8a1ae49a3339f76e1de90e066136
Alejandro Sospedra <alejandro.sospedra@gmail.com>
2022-07-29 14:31:20 +0200

    fix bug in board class

--- src/index.js
+++ src/index.js
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" 
-        onClick={()=> {this.props.onClick()}}>
-          {this.props.value}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
+}
   
 class Board extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-    }
+    };
   }
 
   handleClick(i) {
@@ -29,34 +30,37 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-      return ( <Square value={this.state.square[i]} 
-      onClick={()=> this.handleClick(i) }/>
-      );
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-      const status = 'Next player: X';
+    const status = 'Next player: X';
 
-      return (
+    return (
       <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
+        <div className="status">{status}</div>
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
+        </div>
+        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
+        </div>
+        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          </div>
+        </div>
       </div>
-      );
+    );
   }
 }
 
