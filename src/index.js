14cce7417536c7205dadc21b4d807d85c7e4da46
Alejandro Sospedra <alejandro.sospedra@gmail.com>
2022-08-03 14:15:39 +0200

    add O turn to the game

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -28,8 +29,10 @@ class Board extends React.Component {
   //to determine when a component requires re-rendering.
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares: squares, 
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -42,7 +45,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
